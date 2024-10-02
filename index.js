bded41e4e86c613815e30790af29bedc3e236ac4
rubenavaldez <rubenavaldez@yahoo.com>
2024-10-02 11:51:05 -0700

    added process exit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
 
@@ -6,72 +8,66 @@ async function sortHackerNewsArticles() {
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
-  let articleCount=0
-  let newestDate= 1827834361000
-  let textDate = ""
-
+  let articleCount = 0;
+  let newestDate = Date.now() + 5000;
+  let textDate = "";
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
-  
-  await page.click("text=show")
-  
- 
-  
-  for(let i = 0; i<4; i++){
-    
 
-    let subLines = await page.$$(".subText")
-      
-      for(let j = 0; j < subLines.length; j++){
-          articleCount++
-          if (articleCount > 100){
-            return true
-          }
-          let sub = subLines[j]
-          let ageElem = await sub.$(".age")
-                   
-          let date = await ageElem.getAttribute("title") 
-          let unixTimeStamp = Date.parse(date)
 
-     
+  for (let i = 0; i < 4; i++) {
 
-          let anchor = await ageElem.$("a")
-          let text = await anchor.textContent()
+    let subLines = await page.$$(".subText");
 
-          if( newestDate > unixTimeStamp){
-            console.log("Older article", newestDate , unixTimeStamp , textDate , text)
-            newestDate = unixTimeStamp
-            textDate = text
-          }else{
-            console.error("out of order", newestDate, unixTimeStamp , textDate , text)
-            await browser.close()
-            return false 
-          }
+    for (let j = 0; j < subLines.length; j++) {
 
+      let subline = subLines[j];
+      articleCount++;
+
+      if (articleCount > 100) {
+        return true;
       }
 
-  
+      let ageElem = await subline.$(".age");
+      let date = await ageElem.getAttribute("title");
+      let unixTimeStamp = Date.parse(date);
 
+      let anchor = await ageElem.$("a");
+      let text = await anchor.textContent();
 
-    
-    const isMore = await page.$(".moreLink")
-
-    if(isMore){
-
-      await page.click(".morelink")
-    }else{
-       return true // break loop
+      if (newestDate >= unixTimeStamp) {
+        console.log("Older article", newestDate, unixTimeStamp, textDate, text);
+        newestDate = unixTimeStamp;
+        textDate = text;
+      } else {
+        console.error(
+          "out of order",
+          newestDate,
+          unixTimeStamp,
+          textDate,
+          text
+        );
+        await browser.close();
+        return false;
+      }
     }
 
+    const isMore = await page.$(".morelink");
+
+    if (isMore) {
+      await page.click(".morelink");
+    } else {
+      return true; // break loop
+    }
   }
-  
-  return true
+  await browser.close();
+  return true;
 }
 
 (async () => {
   const result = await sortHackerNewsArticles();
-  
-  console.log(`Evaluation completed. Articles in proper date order: ${result}`);
 
+  console.log(`Evaluation completed. Articles in proper date order: ${result}`);
+  process.exit()
 })();
